b70f71bfceb5e5cfb4020166d5245f08fb19e4e4
Anwesh Marwade <anwesh.marwade@beyondsports.nl>
2021-01-14 23:48:39 +0100

    Added config for insy server

--- src/config.py
+++ src/config.py
@@ -1,3 +1,4 @@
+# FOR GOOGLE COLAB
 # FACE_DATA_DIR = "/content/drive/MyDrive/data/deep_phys/face_videos/"
 # DATA_PATH = "/content/drive/MyDrive/data/rhythmnet/st_maps/"
 # TARGET_SIGNAL_DIR = "/content/drive/MyDrive/data/deep_phys/data_preprocessed/"
@@ -12,20 +13,37 @@
 # lr = 1e-3
 # CLIP_SIZE = 300
 
+# For INSY server
 
-haarcascade_url = "https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_alt2.xml"
-FACE_DATA_DIR = "../data/face_video/"
-TARGET_SIGNAL_DIR = "../data/data_preprocessed/"
-SAVE_CSV_PATH = "kfold.csv"
-ST_MAPS_PATH = "../data/st_maps/"
-CHECKPOINT_PATH = "../checkpoint"
-DATA_PATH = "../data/"
-PLOT_PATH = "../plots"
+# FACE_DATA_DIR = "/content/drive/MyDrive/data/deep_phys/face_videos/"
+HOME_DIR = "/tudelft.net/staff-bulk/ewi/insy/VisionLab/students/amarwade/"
+DATA_PATH = HOME_DIR + "rhythmnet/data/st_maps/"
+TARGET_SIGNAL_DIR = HOME_DIR + "rhythmnet/data/data_preprocessed/"
+SAVE_CSV_PATH = HOME_DIR + "rhythmnet/kfold.csv"
+ST_MAPS_PATH = HOME_DIR + "rhythmnet/data/st_maps/"
+CHECKPOINT_PATH = HOME_DIR + "rhythmnet/checkpoint"
+PLOT_PATH = HOME_DIR + "rhythmnet/plots"
+NUM_WORKERS = 2
+DEVICE = "cuda"
 BATCH_SIZE = 10
 EPOCHS = 20
-CLIP_SIZE = 300
 lr = 1e-3
-IMAGE_WIDTH = 300
-IMAGE_HEIGHT = 75
-NUM_WORKERS = 0
-DEVICE = "cpu"
+CLIP_SIZE = 300
+
+
+# haarcascade_url = "https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_alt2.xml"
+# FACE_DATA_DIR = "../data/face_video/"
+# TARGET_SIGNAL_DIR = "../data/data_preprocessed/"
+# SAVE_CSV_PATH = "kfold.csv"
+# ST_MAPS_PATH = "../data/st_maps/"
+# CHECKPOINT_PATH = "../checkpoint"
+# DATA_PATH = "../data/"
+# PLOT_PATH = "../plots"
+# BATCH_SIZE = 10
+# EPOCHS = 20
+# CLIP_SIZE = 300
+# lr = 1e-3
+# IMAGE_WIDTH = 300
+# IMAGE_HEIGHT = 75
+# NUM_WORKERS = 0
+# DEVICE = "cpu"
